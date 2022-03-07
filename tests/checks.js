f842e78aad67695ddcf768477ee768540fd14933
Álvaro Alonso <aalonsog@dit.upm.es>
2022-03-07 17:47:36 +0100

    Updated tests and readme

--- tests/checks.js
+++ tests/checks.js
@@ -50,7 +50,7 @@ describe("Tests Práctica 5", function() {
             let db_url = `sqlite://${db_file}`;
 
             let bin_path = path.join(PATH_ASSIGNMENT, "bin", "www");
-            server = spawn('npm', ['run', 'super'], {env: {PORT: TEST_PORT, DATABASE_URL: db_url}});
+            server = spawn('npm', [bin_path], {env: {PORT: TEST_PORT, DATABASE_URL: db_url}});
             server.stdout.setEncoding('utf-8');
             server.stdout.on('data', function(data) {
                 log('Salida del servidor: ', data);
