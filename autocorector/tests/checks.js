ec44263f99ac2ea987a5600509e136177d556079
Javier Conde <4b3javi@gmail.com>
2023-03-04 20:46:18 +0100

    Increase timeout to 6000

--- autocorector/tests/checks.js
+++ autocorector/tests/checks.js
@@ -11,7 +11,7 @@ const exec = util.promisify(require("child_process").exec);
 
 const PATH_ASSIGNMENT = path_assignment("blog");
 
-const TIMEOUT =  parseInt(from_env("TIMEOUT", 2000));
+const TIMEOUT =  parseInt(from_env("TIMEOUT", 6000));
 const TEST_PORT =  parseInt(from_env("TEST_PORT", "3001"));
 
 
